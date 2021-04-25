bd9cc4da16c2577e78e3825b4ef6de7337a70549
Ajeloy Catman <ajeloycatman@gmail.com>
2021-04-25 13:56:15 -0400

    Primera versión del prototipo alimentador de mascotas

---
+++ alimentador_mascotas/alimentador_mascotas.ino
@@ -0,0 +1,33 @@
+int force;
+#include <Servo.h> 
+Servo miservo;
+int pos = 0;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(8, OUTPUT);
+  miservo.attach(9);
+  miservo.write(0);
+}
+
+void loop()
+{
+  force = analogRead(A0);
+  if(force == 0){
+    digitalWrite(8, HIGH);
+  }
+  else{
+    digitalWrite(8, LOW);
+    for (pos = 0; pos <= 90; pos += 1) {
+      miservo.write(pos);
+      delay(3);
+    }
+    delay(10000);
+    for (pos = 90; pos >= 0; pos -= 1) {
+      miservo.write(pos);
+      delay(3);
+    }
+  }
+  delay(18000000);
+}
